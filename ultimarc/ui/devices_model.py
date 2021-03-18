61616ff8fc19a7d4004cb4682dde41da52ba92fb
Katie Snow <katiej.snow@gmail.com>
2021-03-18 10:54:03 -0600

    Move DeviceRoles out of the Model class.  Add more meta data to the UIDeviceInfo class.  Setup the meta data for each device and configuration possible.  Use that information in the data method.  Add connected role to know what kind of entry this is.

--- ultimarc/ui/devices_model.py
+++ ultimarc/ui/devices_model.py
@@ -12,17 +12,50 @@ from ultimarc.tools import ToolEnvironmentObject
 
 _logger = logging.getLogger('ultimarc')
 
+UNKNOWN_DEVICE = 'Ultimarc Device'
+
+class DeviceRoles(QMetaEnum):
+    DEVICE_CLASS = 1
+    PRODUCT_NAME = 2
+    PRODUCT_KEY = 3
+    CATEGORY = 4
+    ICON = 5
+    CONNECTED = 6
+
+
 class UIDeviceInfo():
     """ Class fpr holding additional device data for the UI """
+    name = ''
+    class_descr = ''
+    key = ''
     icon = ''
+    connected = True
 
-    def __init__(self):
-        pass
+    def __init__(self, _connected=True, _class_descr = 'Unknown class', _name='Unknown Name', _key=''):
+        self.connected = _connected
+        self.name = _name
+        self.class_descr = _class_descr
+        self.key = _key
+
+        if len(self.class_descr) == 0:
+            self.class_descr = UNKNOWN_DEVICE
+
+    @staticmethod
+    def setup_icon(class_id, device_info):
+        # TODO: Add new images
+        #   Run to get new resource file: pyside6-rcc assets.qrc  -o rc_assets.py
+        if class_id == DeviceClassID.MiniPac.value:
+            device_info.icon = 'qrc:/logos/workstation'
+        else:
+            device_info.icon = 'qrc:/logos/placeholder'
+
+        return device_info
+
 
 class DevicesModel(QAbstractListModel, QObject):
     def __init__(self, args, env: (ToolEnvironmentObject, None)):
         super().__init__()
-        self.class_id = ''
+
         self.args = args
         self.env = env
         self._device_count = self.env.devices.device_count
@@ -36,26 +69,19 @@ class DevicesModel(QAbstractListModel, QObject):
 
     def setup_info(self):
         for dev in self.get_devices():
-            tmp = UIDeviceInfo()
+            tmp = UIDeviceInfo(_name=dev.product_name, _class_descr=dev.class_descr, _key=dev.dev_key)
+            tmp = UIDeviceInfo().setup_icon(dev.class_id, tmp)
+            self._ui_dev_info.append(tmp)
 
-            # TODO: Add new images
-            #   Run to get new resource file: pyside6-rcc assets.qrc  -o rc_assets.py
-            if dev.class_id == DeviceClassID.MiniPac.value:
-                tmp.icon = 'qrc:/logos/workstation'
-            else:
-                tmp.icon = 'qrc:/logos/placeholder'
+        # Configuration Options for non connected devices
+        for device_class in DeviceClassID:
+            tmp = UIDeviceInfo(False, _class_descr=device_class.name)
+            tmp = UIDeviceInfo().setup_icon(device_class, tmp)
             self._ui_dev_info.append(tmp)
 
     def get_devices(self):
         """ Return a list of devices we should show information for. """
         return self.env.devices.filter()
-
-    class DeviceRoles(QMetaEnum):
-        DEVICE_CLASS = 1
-        PRODUCT_NAME = 2
-        PRODUCT_KEY = 3
-        CATEGORY = 4
-        ICON = 5
 
     def roleNames(self):
         # TODO: Add device information to role dict
@@ -64,12 +90,14 @@ class DevicesModel(QAbstractListModel, QObject):
         _key = 'key'.encode('utf-8')
         _category = 'category'.encode('utf-8')
         _icon = 'icon'.encode('utf-8')
+        _connected = 'connected'.encode('utf-8')
         roles = {
-            self.DeviceRoles.DEVICE_CLASS: _class_descr,
-            self.DeviceRoles.PRODUCT_NAME: _name,
-            self.DeviceRoles.PRODUCT_KEY: _key,
-            self.DeviceRoles.CATEGORY: _category,
-            self.DeviceRoles.ICON: _icon
+            DeviceRoles.DEVICE_CLASS: _class_descr,
+            DeviceRoles.PRODUCT_NAME: _name,
+            DeviceRoles.PRODUCT_KEY: _key,
+            DeviceRoles.CATEGORY: _category,
+            DeviceRoles.ICON: _icon,
+            DeviceRoles.CONNECTED: _connected
         }
 
         return roles
@@ -77,48 +105,48 @@ class DevicesModel(QAbstractListModel, QObject):
     def rowCount(self, parent):
         if parent.isValid():
             return 0
-        return self.env.devices.device_count
+        return len(self._ui_dev_info)
 
     def data(self, index: QModelIndex, role):
         if not index.isValid():
             return None
 
-        if role == self.DeviceRoles.DEVICE_CLASS:
+        if role == DeviceRoles.DEVICE_CLASS:
             i = 0
-            for dev in self.get_devices():
+            for ui_dev in self._ui_dev_info:
                 if i == index.row():
-                    return dev.class_descr
+                    return ui_dev.class_descr
                 i = i + 1
 
-        if role == self.DeviceRoles.PRODUCT_NAME:
+        if role == DeviceRoles.PRODUCT_NAME:
             i = 0
-            for dev in self.get_devices():
+            for ui_dev in self._ui_dev_info:
                 if i == index.row():
-                    return dev.product_name
+                    return ui_dev.name
                 i = i + 1
 
-        if role == self.DeviceRoles.PRODUCT_KEY:
+        if role == DeviceRoles.PRODUCT_KEY:
             i = 0
-            for dev in self.get_devices():
+            for ui_dev in self._ui_dev_info:
                 if i == index.row():
-                    return dev.dev_key
+                    return ui_dev.key
                 i = i + 1
 
-        if role == self.DeviceRoles.CATEGORY:
+        if role == DeviceRoles.CATEGORY:
+            return '' if index.row() < self._device_count else 'Ultimarc Configurations'
+
+        if role == DeviceRoles.ICON:
             i = 0
-            for dev in self.get_devices():
+            for ui_dev in self._ui_dev_info:
                 if i == index.row():
-                    return 'device'
+                    return ui_dev.icon
                 i = i + 1
 
-            if i in range(i, i + self.config_count):
-                return DeviceClassID[self.config_count - i]
-
-        if role == self.DeviceRoles.ICON:
+        if role == DeviceRoles.CONNECTED:
             i = 0
-            for dev in self.get_devices():
+            for ui_dev in self._ui_dev_info:
                 if i == index.row():
-                    return self._ui_dev_info[i].icon
+                    return ui_dev.connected
                 i = i + 1
 
         return None
