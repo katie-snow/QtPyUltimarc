450608b06a33b41d22d6ffee114b08b39e706407
Katie Snow <katiej.snow@gmail.com>
2021-11-15 16:44:09 -0700

    Implement methods for writing to device and writing to file for the Mini-pac UI.  Clean up and simplify code in data and setData functions.

--- ultimarc/ui/devices/mini_pac.py
+++ ultimarc/ui/devices/mini_pac.py
@@ -2,16 +2,18 @@
 # This file is subject to the terms and conditions defined in the
 # file 'LICENSE', which is part of this source code package.
 #
+import json
 import logging
 import typing
 
 from PySide6.QtCore import QModelIndex
 
 from ultimarc.devices import DeviceClassID
-from ultimarc.devices.mini_pac import PinMapping
-from ultimarc.system_utils import JSONObject
+from ultimarc.devices.mini_pac import PinMapping, MiniPacDevice
+
 from ultimarc.ui.devices.device import Device
 from ultimarc.ui.device_details_model import DeviceDataRoles
+from ultimarc.system_utils import JSONObject
 
 _logger = logging.getLogger('ultimarc')
 
@@ -21,10 +23,12 @@ class MiniPacUI(Device):
                  device_class_id,
                  name=None, device_class_descr=None,
                  key=''):
-        super(MiniPacUI, self).__init__(args, env, attached, device_class_id, name, device_class_descr, key)
+        super(MiniPacUI, self).__init__(args, env, attached, device_class_id,
+                                        name, device_class_descr, key)
 
         self.icon = 'qrc:/logos/workstation'
         self.config = None
+        self._json_obj = None
 
     def get_description(self):
         return 'This is the description of the Mini-pac device'
@@ -37,15 +41,30 @@ class MiniPacUI(Device):
                                                    address=self.args.address)]
                 for dev in devices:
                     with dev as dev_h:
-                        self.config = JSONObject(dev_h.to_json_str(dev_h.get_current_configuration()))
-                        # _logger.info(self.config)
+                        self.config = dev_h.to_json_str(dev_h.get_current_configuration())
             else:
-                self.config = JSONObject({'schemaVersion': 2.0, 'resourceType': 'mini-pac-pins',
-                                          'deviceClass': self.device_class_id, 'debounce': 'standard', 'pins': []})
+                self.config = {'schemaVersion': 2.0, 'resourceType': 'mini-pac-pins',
+                               'deviceClass': self.device_class_id.value, 'debounce': 'standard', 'pins': []}
+
+            self._json_obj = JSONObject(self.config)
 
     def get_qml(self):
         self.load_config()
         return 'PinDetail.qml'
+
+    def write_device(self):
+        devices = [dev for dev in
+                   self.env.devices.filter(class_id=DeviceClassID.MiniPac, bus=self.args.bus,
+                                           address=self.args.address)]
+        for dev in devices:
+            with dev as dev_h:
+                return dev_h.set_config_ui(self.config)
+
+    def write_file(self, file):
+        if MiniPacDevice.validate_config(self.config, 'mini-pac.schema'):
+            MiniPacDevice.write_to_file(self.config, file.toLocalFile(), indent=2)
+            return True
+        return False
 
     def rowCount(self):
         return len(PinMapping)
@@ -55,58 +74,41 @@ class MiniPacUI(Device):
         if role == DeviceDataRoles.NAME:
             return pin_name
 
-        try:
-            if len(self.config.pins) > 0:
-                for pin in self.config.pins:
-                    if pin.name == pin_name:
-                        if role == DeviceDataRoles.ACTION:
-                            return pin.action
-                        if role == DeviceDataRoles.ALT_ACTION:
-                            try:
-                                return pin.alternate_action
-                            except AttributeError:
-                                return ''
-                        if role == DeviceDataRoles.SHIFT:
-                            try:
-                                return pin.shift
-                            except AttributeError:
-                                return False
-            else:
-                return False if role == DeviceDataRoles.SHIFT else ''
-        except AttributeError:
+        for pin in self._json_obj.pins:
+            if pin.name == pin_name:
+                if role == DeviceDataRoles.ACTION:
+                    return pin.action
+                if role == DeviceDataRoles.ALT_ACTION:
+                    try:
+                        return pin.alternate_action
+                    except AttributeError:
+                        return ''
+                if role == DeviceDataRoles.SHIFT:
+                    try:
+                        return pin.shift
+                    except AttributeError:
+                        return False
+        else:
             return False if role == DeviceDataRoles.SHIFT else ''
 
     def setData(self, index: QModelIndex, value, role: int = ...):
         pin_name = list(PinMapping)[index.row()]
-        cur_pin = None
 
-        try:
-            if len(self.config.pins) > 0:
-                for pin in self.config.pins:
-                    if pin.name == pin_name:
-                        cur_pin = pin
-                        break
-        except AttributeError:
-            pass
-
-        update_pin = False
-        if cur_pin is None:
-            cur_pin = JSONObject({'name': pin_name, 'action': '', 'alternate_action': '', 'shift': False})
+        for pin in self.config['pins']:
+            if pin['name'] == pin_name:
+                break
+        else:
+            pin = {'name': pin_name, 'action': ''}
+            self.config['pins'].append(pin)
 
         if role == DeviceDataRoles.SHIFT:
-            cur_pin.shift = value
-            update_pin = True
+            pin['shift'] = value
 
         if role == DeviceDataRoles.ACTION:
-            cur_pin.action = value
-            update_pin = True
+            pin['action'] = value
 
         if role == DeviceDataRoles.ALT_ACTION:
-            cur_pin.alternate_action = value
-            update_pin = True
+            pin['alternate_action'] = value
 
-        if update_pin:
-            self.config.pins.append(cur_pin)
-            return True
-
-        return False
+        self._json_obj = JSONObject(self.config)
+        return True
