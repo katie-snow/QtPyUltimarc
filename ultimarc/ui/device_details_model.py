ba3e4ec46658250e64ed5094673d42a5feb40852
Katie Snow <katiej.snow@gmail.com>
2021-12-08 17:24:55 -0700

    Rename device to _device to allow Property to be named device.  Remove debounce functionality.  Now placed in mini-pac.

--- ultimarc/ui/device_details_model.py
+++ ultimarc/ui/device_details_model.py
@@ -7,53 +7,47 @@ import logging
 import typing
 from collections import OrderedDict
 
-from PySide6.QtCore import QObject, QAbstractListModel, QModelIndex, Signal, Property
+from PySide6.QtCore import QObject, QAbstractListModel, QModelIndex, Property
 
 _logger = logging.getLogger('ultimarc')
 
 
 class DeviceDataModel(QAbstractListModel, QObject):
     """ Model for populating the QML Repeater in the UI """
-    _changed_debounce_ = Signal(str)
 
     def __init__(self):
         super().__init__()
-        self.device = None
+        self._device = None
 
     def roleNames(self) -> typing.Dict:
         roles = OrderedDict()
-        for k, v in self.device.roleNames():
+        for k, v in self._device.roleNames():
             roles[k] = v.encode('utf-8')
         return roles
 
     def rowCount(self, parent: QModelIndex = ...) -> int:
-        return self.device.rowCount() if self.device else 0
+        return self._device.rowCount() if self._device else 0
 
     def data(self, index: QModelIndex, role: int = ...) -> typing.Any:
         if not index.isValid():
             return None
 
-        return self.device.data(index, role)
+        return self._device.data(index, role)
 
     def set_device(self, device):
         self.beginResetModel()
-        self.device = device
+        self._device = device
         self.endResetModel()
 
     def setData(self, index: QModelIndex, value, role: int = ...):
         if not index.isValid():
             return None
 
-        val = self.device.setData(index, value, role)
+        val = self._device.setData(index, value, role)
         self.dataChanged.emit(index, index, [])
         return val
 
-    def get_debounce(self):
-        return self.device.get_debounce() if self.device is not None else None
+    def get_device(self):
+        return self._device
 
-    def set_debounce(self, debounce):
-        if self.device:
-            self.device.set_debounce(debounce)
-            self._changed_debounce_.emit(debounce)
-
-    debounce = Property(str, get_debounce, set_debounce, notify=_changed_debounce_)
+    device = Property(QObject, get_device, constant=True)
