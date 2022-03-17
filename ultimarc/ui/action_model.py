7ce8c170bd929d7fadc3509fe8b09b4ca08956d7
Katie Snow <katiej.snow@gmail.com>
2022-03-16 23:17:37 -0600

    Add functionality for updating the list with macro entries.
    Remove legacy variable, not used.

--- ultimarc/ui/action_model.py
+++ ultimarc/ui/action_model.py
@@ -14,10 +14,17 @@ _logger = logging.getLogger('ultimarc')
 
 
 class ActionModel(QAbstractListModel, QObject):
-    def __init__(self, legacy=False):
+    def __init__(self):
         super().__init__()
+        self.macro_names = []
 
-        self.legacy = legacy
+    def set_macro_names(self, macros):
+        self.beginResetModel()
+        names = []
+        for macro in macros:
+            names.append(macro['name'])
+        self.macro_names = names
+        self.endResetModel()
 
     class ActionModelRoles(QMetaEnum):
         TEXTROLE = 1
@@ -29,11 +36,15 @@ class ActionModel(QAbstractListModel, QObject):
         return roles
 
     def rowCount(self, parent: QModelIndex = ...) -> int:
-        return len(IPACSeriesMapping) if not self.legacy else 0
+        return len(IPACSeriesMapping) + len(self.macro_names)
 
     def data(self, index: QModelIndex, role: int = ...) -> typing.Any:
         if not index.isValid():
             return None
 
         if role == self.ActionModelRoles.TEXTROLE:
-            return list(IPACSeriesMapping)[index.row()] if not self.legacy else None
+            map_list = list(IPACSeriesMapping)
+            if index.row() >= len(map_list):
+                return self.macro_names[index.row() - len(map_list)]
+            else:
+                return map_list[index.row()]
