e1d4aed978f975f7f9182172d4ee4ff7455655bd
Katie Snow <katiej.snow@gmail.com>
2024-02-23 11:22:43 -0700

    Testing of data population complete.  Added initial tool functionality and update mapping to handle the trackball values

---
+++ ultimarc/tools/ultimateio.py
@@ -0,0 +1,151 @@
+#
+# This file is subject to the terms and conditions defined in the
+# file 'LICENSE', which is part of this source code package.
+#
+# Template for Ultimarc CLI tools.
+#
+import logging
+import os
+import sys
+
+from ultimarc import translate_gettext as _
+from ultimarc.devices import DeviceClassID
+from ultimarc.tools import ToolContextManager, ToolEnvironmentObject
+
+
+_logger = logging.getLogger('ultimarc')
+
+# Tool_cmd and tool_desc name are required.
+# Remember to add/update bash completion in 'tools.bash'
+tool_cmd = _('ultimateio')
+tool_desc = _('Manage UltimateIO devices')
+
+
+class UltimateIOClass(object):
+    def __init__(self, args, tool_env: ToolEnvironmentObject):
+        """
+        :param args: command line arguments.
+        :param tool_env: tool environment information, see: gcp_initialize().
+        """
+        self.args = args
+        self.tool_env = tool_env
+
+    def run(self):
+        """
+        Main program process
+        :return: Exit code value
+        """
+
+        # Get devices we want to work with based on filters.
+        devices = [dev for dev in
+                   self.tool_env.devices.filter(class_id=DeviceClassID.UltimateIO, bus=self.args.bus,
+                                                address=self.args.address)]
+
+        if not devices:
+            _logger.error(_('No ultimateIO devices found, aborting'))
+            return -1
+
+        # Get config from device
+        if self.args.get_config:
+            for dev in devices:
+                with dev as dev_h:
+                    indent = int(self.args.indent) if self.args.indent else None
+                    response = dev_h.get_device_config(indent, self.args.file)
+                    _logger.info(response)
+
+        # Set ultimateIO device configuration from a configuration file
+        if self.args.set_config:
+            for dev in devices:
+                with dev as dev_h:
+                    use_current = self.args.current
+                    dev_h.set_config(self.args.set_config, use_current)
+                    _logger.info(f'{dev.dev_key} ({dev.bus},{dev.address}): ' +
+                                 _('configuration successfully applied to device.'))
+
+        if self.args.set_pin:
+            for dev in devices:
+                with dev as dev_h:
+                    dev_h.set_pin(self.args.set_pin)
+                    _logger.info(f'{dev.dev_key} ({dev.bus},{dev.address}): ' +
+                                 _('pin successfully applied to device.'))
+
+        # Set debounce value
+        if self.args.set_debounce:
+            for dev in devices:
+                with dev as dev_h:
+                    response = dev_h.set_debounce(self.args.set_debounce)
+                    if response:
+                        _logger.info(f'{dev.dev_key} ({dev.bus},{dev.address}): ' +
+                                     _('debounce successfully applied to device.'))
+
+        # Set X Threshold value
+        if self.args.set_x_threshold:
+            for dev in devices:
+                with dev as dev_h:
+                    response = dev_h.set_x_threshold(self.args.set_x_threshold)
+                    if response:
+                        _logger.info(f'{dev.dev_key} ({dev.bus},{dev.address}): ' +
+                                     _('x threshold successfully applied to device.'))
+
+        # Set Y Threshold value
+        if self.args.set_y_threshold:
+            for dev in devices:
+                with dev as dev_h:
+                    response = dev_h.set_y_threshold(self.args.set_y_threshold)
+                    if response:
+                        _logger.info(f'{dev.dev_key} ({dev.bus},{dev.address}): ' +
+                                     _('y threshold successfully applied to device.'))
+
+        return 0
+
+
+def run():
+    # Set global debug value and setup application logging.
+    ToolContextManager.initialize_logging(tool_cmd)
+    parser = ToolContextManager.get_argparser(tool_cmd, tool_desc)
+    group = parser.add_argument_group()
+
+    # TODO:  Setup additional program arguments here.
+    group.add_argument('--get-config', help=_('Get ultimateIO device config'), default=False, action='store_true')
+    group.add_argument('--indent', help=_('Set the json indent level for the output of the device configuration'),
+                       default=None, metavar='INT')
+    group.add_argument('--file', help=_('File path to write out the json of the device configuration'), type=str,
+                       default=None, metavar='FILE-NAME')
+    group.add_argument('--set-config', help=_('Set ultimateIO device config from config file'), type=str, default=None,
+                       metavar='CONFIG-FILE')
+    group.add_argument('--current',
+                       help=_('Use ultimateIO current config when applying config from file'), default=False,
+                       action='store_true')
+    group.add_argument('--set-debounce', help=_('Set ultimateIO debounce value'), type=str, metavar='STR')
+    group.add_argument('--set-xthreshold', help=_('Set ultimateIO X threshold value'), type=int, metavar='INT')
+    group.add_argument('--set-ythreshold', help=_('Set ultimateIO Y threshold value'), type=int, metavar='INT')
+    group.add_argument('--set-pin', help=_('Set single pin'), type=str,
+                       default=None, metavar=('PIN', 'ACTION', 'ALT_ACTION', 'IS_SHIFT'), nargs=4)
+    args = parser.parse_args()
+
+    if not args.get_config and args.indent:
+        _logger.error(_('The --indent argument can only be used with the --get_config argument '))
+        return -1
+    if not args.get_config and args.file:
+        _logger.error(_('The --file argument can only be used with the --get_config argument '))
+        return -1
+    if not args.set_config and args.current:
+        _logger.error(_('The --current argument can only be used with the --set_config argument '))
+        return -1
+
+    if args.set_config:
+        # Always force absolute path for config files.
+        args.set_config = ToolContextManager.clean_config_path(args.set_config)
+        if not os.path.exists(args.set_config):
+            _logger.error(_('Unable to find configuration file specified in argument.'))
+            return -1
+
+    with ToolContextManager(tool_cmd, args) as tool_env:
+        process = UltimateIOClass(args, tool_env)
+        exit_code = process.run()
+        return exit_code
+
+
+# --- Main Program Call ---
+if __name__ == "__main__":
+    sys.exit(run())
